5bbc000bac55dae7a66a157966c684b66a92ae0f
ethanlsry <e.lissroy@yahoo.com>
2024-01-17 14:01:06 -0500

    formatting

--- Weather.js
+++ Weather.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  //SECTION 1: MAPBOX CODE
-
-  //set up mapbox
+  //MAPBOX CODE
   mapboxgl.accessToken = 'pk.eyJ1IjoibW9udG1vcmVuY3k2IiwiYSI6ImNsMTNzaGM3bTBqY24zcXBka3lkdG96OGQifQ.7N0btgH56ImGUyFhElJoRg';
   const map = new mapboxgl.Map({
       container: 'map', // container ID
@@ -70,119 +68,111 @@ document.addEventListener("DOMContentLoaded", function(){
   });
 
 
+//WEATHER CODE
 
+//initialize reader in global scope for parsing JSON code
+let reader;
 
-
-  
-
-    //SECTION 2: WEATHER CODE
-
-    //initialize reader in global scope for parsing JSON code- try initializing in local
-    let reader;
-
-    function fetchWeather(lat, long, units){
-      console.log("fetching weather now!!!")
-      console.log(units);
-      //COPY AND PASTE WEATHER FETCHING STUFF HERE
-      //takes lat and long; sends that to weather API
-      fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=1f14404c7113a7cf664c056a8cfcf176&units=" + units)
-      .then(response => response.body)
-      .then(body => {
-          reader = body.getReader();
-          return new ReadableStream({
-            start(controller) {
-              // The following function handles each data chunk
-              function push() {
-                // "done" is a Boolean and value a "Uint8Array"
-                reader.read().then( ({done, value}) => {
-                  // If there is no more data to read
-                  if (done) {
-                    controller.close();
-                    return;
-                  }
-                  // Get the data and send it to the browser via the controller
-                  controller.enqueue(value);
-                  push();
-                })
+function fetchWeather(lat, long, units){
+  console.log("fetching weather now!!!")
+  console.log(units);
+  //takes lat and long; sends that to weather API
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=1f14404c7113a7cf664c056a8cfcf176&units=" + units)
+  .then(response => response.body)
+  .then(body => {
+      reader = body.getReader();
+      return new ReadableStream({
+        start(controller) {
+          // The following function handles each data chunk
+          function push() {
+            // "done" is a Boolean and value a "Uint8Array"
+            reader.read().then( ({done, value}) => {
+              // If there is no more data to read
+              if (done) {
+                controller.close();
+                return;
               }
+              // Get data and send it to the browser via controller
+              controller.enqueue(value);
               push();
-            }
-          })
-        })
-        .then(stream => {
-          // Respond with our stream
-          return new Response(stream, { headers: { "Content-Type": "text/html" } }).text();
-        })
-        .then(result => readWeatherAPI(result, units))
+            })
+          }
+          push();
+        }
+      })
+    })
+    .then(stream => {
+      // Respond with our stream
+      return new Response(stream, { headers: { "Content-Type": "text/html" } }).text();
+    })
+    .then(result => readWeatherAPI(result, units))
+}
+
+function readWeatherAPI(data, units){
+    //convert readable stream response into JSON
+    let json_result = JSON.parse(data);
+
+    //parse JSON for data
+    let location = json_result.name;
+
+    let description = json_result.weather[0].description;
+
+    let actual_temp = json_result.main.temp;
+    let max_temp = json_result.main.temp_max;
+    let min_temp = json_result.main.temp_min;
+    let sunrise = json_result.sys.sunrise;
+    let sunset = json_result.sys.sunset;
+    let wind_speed = json_result.wind.speed;
+
+    function convert_UNIX_epoch_to_time(UNIX_time){
+      let time_in_mili = UNIX_time * 1000;
+      let date_obj = new Date (time_in_mili);
+      let time_formatted = date_obj.toLocaleString('en-US', {hour: "numeric",  minute: "numeric"})
+      return time_formatted;
     }
 
-    function readWeatherAPI(data, units){
-        //convert readable stream response into JSON
-        let json_result = JSON.parse(data);
+    //set temperature units
+    let temp_units;
+    let wind_units;
 
-        //parse JSON for data
-        let location = json_result.name;
-
-        let description = json_result.weather[0].description;
-
-        let actual_temp = json_result.main.temp;
-        let max_temp = json_result.main.temp_max;
-        let min_temp = json_result.main.temp_min;
-        let sunrise = json_result.sys.sunrise;
-        let sunset = json_result.sys.sunset;
-        let wind_speed = json_result.wind.speed;
-
-        function convert_UNIX_epoch_to_time(UNIX_time){
-          //ACCOUNT FOR TIME ZONE!!!!!!!!!
-          let time_in_mili = UNIX_time * 1000;
-          let date_obj = new Date (time_in_mili);
-          let time_formatted = date_obj.toLocaleString('en-US', {hour: "numeric",  minute: "numeric"})
-          return time_formatted;
-        }
-
-
-        //set temperature units
-        let temp_units;
-        let wind_units;
-
-        if (units == "imperial"){
-          temp_units = "°F";
-          wind_units = " mph"
-        } else if (units == "metric"){
-          temp_units = "°C";
-          wind_units = " m/s";
-        }
-
-        //output JSON data
-        console.log("location: " + location)
-        console.log("actual_temp: " + actual_temp.toFixed(1) + temp_units)
-        console.log("max_temp: " + max_temp.toFixed(1) + temp_units)
-        console.log("min_temp: " + min_temp.toFixed(1) + temp_units)
-        console.log("sunrise: " + convert_UNIX_epoch_to_time(sunrise))
-        console.log("sunset: " + convert_UNIX_epoch_to_time(sunset))
-        console.log("wind_speed: " + wind_speed + wind_units)
-
-        //update front-end with weather data
-          //reset text for 
-          let location_text = document.getElementById("location");
-          location_text.textContent = "location: " + location;
-
-          let actual_temp_text = document.getElementById("actual_temp");
-          actual_temp_text.textContent = "current temp: " + actual_temp.toFixed(1) + temp_units;
-
-          let wind_speed_text = document.getElementById("wind_speed");
-          wind_speed_text.textContent = "wind speed: "+ wind_speed + wind_units;
-
-          let max_temp_text = document.getElementById("max_temp");
-          max_temp_text.textContent = "max temp: " +  max_temp.toFixed(1) + temp_units;
-
-          let min_temp_text = document.getElementById("min_temp");
-          min_temp_text.textContent = "min temp: " + min_temp.toFixed(1) + temp_units;
-
-          let sunrise_text = document.getElementById("sunrise");
-          sunrise_text.textContent = "sunrise: " + convert_UNIX_epoch_to_time(sunrise) + " EST";
-
-          let sunset_text = document.getElementById("sunset");
-          sunset_text.textContent = "sunset: " + convert_UNIX_epoch_to_time(sunset) + ' EST';
+    if (units == "imperial"){
+      temp_units = "°F";
+      wind_units = " mph"
+    } else if (units == "metric"){
+      temp_units = "°C";
+      wind_units = " m/s";
     }
+
+    //output JSON data
+    console.log("location: " + location)
+    console.log("actual_temp: " + actual_temp.toFixed(1) + temp_units)
+    console.log("max_temp: " + max_temp.toFixed(1) + temp_units)
+    console.log("min_temp: " + min_temp.toFixed(1) + temp_units)
+    console.log("sunrise: " + convert_UNIX_epoch_to_time(sunrise))
+    console.log("sunset: " + convert_UNIX_epoch_to_time(sunset))
+    console.log("wind_speed: " + wind_speed + wind_units)
+
+    //update front-end with weather data
+    //reset text for 
+    let location_text = document.getElementById("location");
+    location_text.textContent = "location: " + location;
+
+    let actual_temp_text = document.getElementById("actual_temp");
+    actual_temp_text.textContent = "current temp: " + actual_temp.toFixed(1) + temp_units;
+
+    let wind_speed_text = document.getElementById("wind_speed");
+    wind_speed_text.textContent = "wind speed: "+ wind_speed + wind_units;
+
+    let max_temp_text = document.getElementById("max_temp");
+    max_temp_text.textContent = "max temp: " +  max_temp.toFixed(1) + temp_units;
+
+    let min_temp_text = document.getElementById("min_temp");
+    min_temp_text.textContent = "min temp: " + min_temp.toFixed(1) + temp_units;
+
+    let sunrise_text = document.getElementById("sunrise");
+    sunrise_text.textContent = "sunrise: " + convert_UNIX_epoch_to_time(sunrise) + " EST";
+
+    let sunset_text = document.getElementById("sunset");
+    sunset_text.textContent = "sunset: " + convert_UNIX_epoch_to_time(sunset) + ' EST';
+}
 }, false);
